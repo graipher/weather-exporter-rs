a305937431ace7b9df6ab30a902ec5095165a6a4
Andreas Weiden <andreas.weiden@gmail.com>
2024-02-09 14:27:14 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,51 @@
+# syntax=docker/dockerfile:1.3-labs
+
+FROM rust:1.75.0-slim-bookworm AS builder
+
+RUN update-ca-certificates
+ENV USER=worker
+ENV UID=1001
+
+RUN adduser \
+    --disabled-password \
+    --gecos "" \
+    --home "/nonexistent" \
+    --shell "/sbin/nologin" \
+    --no-create-home \
+    --uid "${UID}" \
+    "${USER}"
+
+RUN apt-get update \
+    && apt-get -y upgrade \
+    && apt-get -y --no-install-recommends install pkg-config make g++ libssl-dev \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+
+# Create an empty src/main.rs, so compilation of dependencies works without errors
+RUN cargo init --vcs none
+COPY ./Cargo.toml ./Cargo.lock /app/
+# This step compiles only our dependencies and saves them in a layer. This is the most impactful time savings
+# Note the use of --mount=type=cache. On subsequent runs, we'll have the crates already downloaded
+RUN --mount=type=cache,target=/usr/local/cargo/registry cargo build --release
+
+COPY ./src /app/src/
+# A bit of magic here!
+# * We're mounting that cache again to use during the build, otherwise it's not present and we'll have to download those again - bad!
+# * Rust here is a bit fiddly, so we'll touch the files (even though we copied over them) to force a new build
+RUN --mount=type=cache,target=/usr/local/cargo/registry touch /app/src/main.rs && cargo build --release
+
+FROM gcr.io/distroless/cc-debian12
+
+COPY --from=builder /etc/passwd /etc/passwd
+COPY --from=builder /etc/group /etc/group
+
+WORKDIR /app
+
+COPY --from=builder /app/target/release/weather-exporter-rs ./
+USER worker:worker
+
+EXPOSE $PORT
+
+CMD ["/app/weather-exporter-rs"]
---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use std::collections::HashMap;
+use std::env;
+use std::time::{Duration, SystemTime};
+
+use prometheus_exporter::{
+    self,
+    prometheus::register_gauge_vec,
+};
+use prometheus_exporter::prometheus::register_gauge;
+use reqwest::Client;
+use serde::Deserialize;
+
+static URL: &str = "https://api.openweathermap.org/data/2.5/weather";
+
+
+#[derive(Debug, Deserialize)]
+struct Weather {
+    temp: f32,
+    pressure: u16,
+    humidity: u8,
+}
+
+#[derive(Debug, Deserialize)]
+struct OpenWeatherMapData {
+    main: Weather,
+}
+
+async fn get_weather(client: &Client, params: &HashMap<&str, String>) -> Result<OpenWeatherMapData, reqwest::Error> {
+    let response = client.get(URL).query(&params).send().await?;
+    let json = response.json::<OpenWeatherMapData>().await?;
+    Ok(json)
+}
+
+#[tokio::main]
+async fn main() {
+    let port = env::var("PORT").or::<String>(Ok("9185".to_string())).unwrap();
+    println!("Listening on port :{}", port);
+    let binding = format!("0.0.0.0:{}", port).parse().unwrap();
+    let exporter = prometheus_exporter::start(binding).unwrap();
+
+    let mut params: HashMap<&str, String> = HashMap::new();
+    params.insert("lat", env::var("LAT").or::<String>(Ok("50.90980300120387".to_string())).unwrap().to_owned());
+    params.insert("lon", env::var("LON").or::<String>(Ok("6.812524218404036".to_string())).unwrap().to_owned());
+    params.insert("units", env::var("UNITS").or::<String>(Ok("metric".to_string())).unwrap().to_owned());
+    params.insert("appid", env::var("APPID").or::<String>(Ok("f29f74c2c056183640accbbb124e6c3f".to_string())).unwrap().to_owned());
+    let client = Client::new();
+
+    let temperature = register_gauge_vec!("weather_temperature", "Outside temperature in °C", &["city"]).unwrap();
+    let humidity = register_gauge_vec!("weather_humidity", "Outside humidity in %", &["city"]).unwrap();
+    let pressure = register_gauge_vec!("weather_pressure", "Outside pressure in hPa", &["city"]).unwrap();
+    let last_updated = register_gauge_vec!("weather_last_updated", "Last update of weather", &["city"]).unwrap();
+    let process_start_time = register_gauge!("process_start_time_seconds", "Start time of the process").unwrap();
+
+    let mut now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+    process_start_time.set(now as f64);
+
+    let city = "Frechen";
+
+    loop {
+        match get_weather(&client, &params).await {
+            Ok(data) => {
+                now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+                println!("time={}, temperature={}, humidity={}, pressure={}", now, data.main.temp, data.main.humidity, data.main.pressure);
+                temperature.get_metric_with_label_values(&[city]).unwrap().set(data.main.temp as f64);
+                humidity.get_metric_with_label_values(&[city]).unwrap().set(data.main.humidity as f64);
+                pressure.get_metric_with_label_values(&[city]).unwrap().set(data.main.pressure as f64);
+                last_updated.get_metric_with_label_values(&[city]).unwrap().set(now as f64);
+            }
+            Err(err) => eprintln!("{}", err)
+        }
+        let _guard = exporter.wait_duration(Duration::from_secs(10 * 60));
+    }
+}
